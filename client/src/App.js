9f9ae828f79645532b11675e61ef9667cf4217d6
talish786 <jam.talish@gmail.com>
2023-05-20 02:08:31 +0500

    install concurrently and react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
+import Routing from "./routes/Routing";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routing />
   );
 }
 export default App;
---
+++ client/src/screens/auth/AdminLogin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminLogin = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold">
+            Admin Login
+        </h1>
+    </div>
+  )
+}
+
+export default AdminLogin
